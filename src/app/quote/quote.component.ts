c54d9dea30dfd68ec7630e707216275e413fd1da
Dickson Karis <dicksonkariuki4@gmail.com>
2019-09-21 21:50:53 +0300

    create quotes component

---
+++ src/app/quote/quote.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from "@angular/core";
+import { Quote } from "../quote";
+
+@Component({
+  selector: "app-quote",
+  templateUrl: "./quote.component.html",
+  styleUrls: ["./quote.component.css"]
+})
+export class QuoteComponent implements OnInit {
+  quotes = [
+    new Quote(
+      1,
+      "Ellen White",
+      "Talk unbelief, and you will have unbelief; but talk faith, and you will have faith.",
+      new Date(2019.09, 21)
+    )
+  ];
+  deleteQuote(isComplete, index) {
+    if (isComplete) {
+      let toDelete = confirm("Are you sure you want to delete quote");
+      if (toDelete) {
+        this.quotes.splice(index, 1);
+      }
+    }
+  }
+
+  addNewQuote(quote) {
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength + 1;
+    quote.completeDate = new Date(quote.completeDate);
+    this.quotes.push(quote);
+  }
+
+  upVotes: number = 0;
+  upVote() {
+    this.upVotes = this.upVotes + 1;
+  }
+  downVotes: number = 0;
+  downVote() {
+    this.downVotes = this.downVotes + 1;
+  }
+  constructor() {}
+
+  ngOnInit() {}
+}
