256755c0c1ef2d52942d15dcc47dc07bc3870b3e
Dickson Karis <dicksonkariuki4@gmail.com>
2019-09-22 06:26:24 +0300

    Add input binding to the form

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import { Quote } from "../quote";
 
 @Component({
-  selector: 'app-quote-form',
-  templateUrl: './quote-form.component.html',
-  styleUrls: ['./quote-form.component.css']
+  selector: "app-quote-form",
+  templateUrl: "./quote-form.component.html",
+  styleUrls: ["./quote-form.component.css"]
 })
 export class QuoteFormComponent implements OnInit {
+  newQuote = new Quote(0, "", "", new Date());
+  @Output() addQuote = new EventEmitter<Quote>();
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {}
+  submitQuote() {
+    this.addQuote.emit(this.newQuote);
   }
 
+  ngOnInit() {}
 }
